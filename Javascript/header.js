d4b3f5086a103ea487e7f5da8bd3691ce2d247ce
sonupooja <sonupooja2398@gmail.com>
2021-12-28 22:12:32 +0530

    Discavery land

---
+++ Javascript/header.js
@@ -0,0 +1,16 @@
+document.querySelector('#header').innerHTML = `
+<div class="header">
+<div class="logo">
+    <div class="logo-name">
+        <span class="logo-text1">Discovery </span>
+        <span class="logo-text2">land company</span> 
+    </div>
+</div>
+    <div class="options">
+        <div>About DLC</div>
+        <div>Our Worlds</div>
+        <div>Experiences</div>
+        <div>Gallery</div>
+        <div>Press</div>  
+</div>
+</div>`;
